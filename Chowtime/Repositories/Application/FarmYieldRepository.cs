d136a1c92c778e3498544177afc30e26779f579a
HarperJeremy <harper@stimulantgroup.com>
2015-10-26 18:11:53 -0500

    Forgot repos

---
+++ Chowtime/Repositories/Application/FarmYieldRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Security.Cryptography;
+using System.Text;
+using SGApp.Utility;
+using SGApp.Models.EF;
+
+namespace SGApp.Repository.Application
+{
+    public class FarmYieldRepository : RepositoryBase<FarmYield>
+    {
+        public override System.Linq.IQueryable<FarmYield> EntityCollection
+        {
+            get
+            {
+                return DbContext.FarmYields.AsQueryable();
+            }
+        }
+
+        protected override FarmYield DeleteRecord(FarmYield entity)
+        {
+            DbContext.FarmYields.Remove(entity);
+            DbContext.SaveChanges();
+            return entity;
+        }
+
+        protected override FarmYield InsertRecord(FarmYield entity)
+        {
+            DbContext.FarmYields.Add(entity);
+            DbContext.SaveChanges();
+            return entity;
+        }
+
+        protected override FarmYield UpdateRecord(FarmYield entity)
+        {
+            DbContext.SaveChanges();
+            return entity;
+        }
+
+
+        public override List<FarmYield> GetByPredicate(string predicate)
+        {
+            var iq = DbContext.FarmYields.AsQueryable();
+            return predicate.Length > 0 ? iq.Where(predicate, null).ToList() : iq.ToList();
+        }
+
+        public List<FarmYield> GetFarmYields()
+        {
+
+            return DbContext.FarmYields
+            .OrderBy(x => x.YieldDate).ToList();
+        }
+
+
+        public override FarmYield GetById(int id)
+        {
+            return DbContext.FarmYields.Where(x => x.YieldID == id).SingleOrDefault();
+        }
+
+        public decimal? GetPondWeightByDate(DateTime reportDate)
+        {
+            DateTime endDate = reportDate.AddDays(2);
+            return DbContext.FarmYields.Where(x => x.YieldDate > reportDate && x.YieldDate < endDate).Sum(x => x.PoundsHeaded);
+        }
+
+        public List<FarmYield> GetByDate(DateTime reportDate)
+        {
+            DateTime endDate = reportDate.AddDays(2);
+            return DbContext.FarmYields.Where(x => x.YieldDate > reportDate && x.YieldDate < endDate).ToList();
+        }
+    }
+
+
+}
---
+++ Chowtime/Repositories/Application/ShiftEndRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Security.Cryptography;
+using System.Text;
+using SGApp.Utility;
+using SGApp.Models.EF;
+
+namespace SGApp.Repository.Application
+{
+    public class ShiftEndRepository : RepositoryBase<ShiftEnd>
+    {
+        public override System.Linq.IQueryable<ShiftEnd> EntityCollection
+        {
+            get
+            {
+                return DbContext.ShiftEnds.AsQueryable();
+            }
+        }
+
+        protected override ShiftEnd DeleteRecord(ShiftEnd entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override ShiftEnd InsertRecord(ShiftEnd entity)
+        {
+            DbContext.ShiftEnds.Add(entity);
+            DbContext.SaveChanges();
+            return entity;
+        }
+
+        protected override ShiftEnd UpdateRecord(ShiftEnd entity)
+        {
+            DbContext.SaveChanges();
+            return entity;
+        }
+
+
+        public override List<ShiftEnd> GetByPredicate(string predicate)
+        {
+            var iq = DbContext.ShiftEnds.AsQueryable();
+            return predicate.Length > 0 ? iq.Where(predicate, null).ToList() : iq.ToList();
+        }
+
+        public List<ShiftEnd> GetShiftEnds()
+        {
+
+            return DbContext.ShiftEnds
+            .OrderBy(x => x.ShiftDate).ToList();
+        }
+
+
+        public override ShiftEnd GetById(int id)
+        {
+            return DbContext.ShiftEnds.Where(x => x.ShiftEndID == id).SingleOrDefault();
+        }
+
+        public ShiftEnd GetByDate(DateTime reportDate)
+        {
+            DateTime endDate = reportDate.AddDays(2);
+            return DbContext.ShiftEnds.Where(x => x.ShiftDate > reportDate && x.ShiftDate < endDate).FirstOrDefault();
+        }
+    }
+
+
+}
